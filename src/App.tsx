f91bb9b65ff1626089be84a31985ef0b7b478df4
Marion Schimmerling <marion.schimmerling2@hotmail.fr>
2024-07-18 11:01:01 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Layout from './components/Layout.js'
+import Recipes from './components/Recipes.js'
+
+export default function App() {
+  return (
+    <>
+      <Layout><Recipes /></Layout>
+    </>
+  )
+}
+
+
